ee2adddc2e55d537ce06e3d6082d5d471ba9b6c1
Sergey Chaban <sergey.chaban@gmail.com>
2021-11-27 18:07:36 +0200

    enable GLSL echo;

--- src/cmd/roof_rpi.sh
+++ src/cmd/roof_rpi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./crosscore_demo -demo:roof -draw:ogl -bump:0 -spec:0 -smap:256 -tlod_bias:0 -nwrk:2 -lowq:1 -vl:1 -adapt:4 -gpu_preload:1 -w:720 -h:480 $*
+./crosscore_demo -demo:roof -draw:ogl -bump:0 -spec:0 -smap:256 -tlod_bias:0 -nwrk:2 -lowq:1 -vl:1 -adapt:4 -gpu_preload:1 -glsl_echo:1 -w:720 -h:480 $*
