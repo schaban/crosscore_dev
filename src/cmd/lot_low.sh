65aaa6d1ed693e0e1a2f255536acbf49b80e4c15
Sergey Chaban <sergey.chaban@gmail.com>
2021-12-07 10:28:17 +0200

    use adapt:1 mode in standard run-scripts;

--- src/cmd/lot_low.sh
+++ src/cmd/lot_low.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./crosscore_demo -demo:lot -draw:ogl -bump:0 -spec:0 -w:720 -h:480 -smap:256 -tlod_bias:0 -vl:1 -lowq:1 $*
+./crosscore_demo -demo:lot -draw:ogl -bump:0 -spec:0 -w:720 -h:480 -smap:256 -tlod_bias:0 -vl:1 -lowq:1 -adapt:1 $*
--- src/cmd/roof_low.sh
+++ src/cmd/roof_low.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./crosscore_demo -demo:roof -draw:ogl -bump:0 -spec:0 -w:1000 -h:600 -smap:512 -tlod_bias:0 -lowq:1 $*
+./crosscore_demo -demo:roof -draw:ogl -bump:0 -spec:0 -w:1000 -h:600 -smap:512 -tlod_bias:0 -lowq:1 -adapt:1 $*
