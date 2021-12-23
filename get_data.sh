0ab42a8306b323f980d985ca08338b3d54fd3274
Sergey Chaban <sergey.chaban@gmail.com>
2021-12-23 13:46:41 +0200

    data downloader: + portability;

--- get_data.sh
+++ get_data.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-wget -q -O - https://schaban.github.io/crosscore_web_demo/xcore_web.tar.xz | (tar --strip-components=1 -xvJf - xcore_web/bin/data)
+XDATA_URL="https://schaban.github.io/crosscore_web_demo/xcore_web.tar.xz"
+XDATA_BIN="xcore_web/bin"
+XDATA_DIR="$XDATA_BIN/data"
+
+if [ "$(uname -s)" = "Linux" ]; then
+	wget -q -O - $XDATA_URL | (tar --strip-components=1 -xvJf - $XDATA_DIR)
+else
+	if [ ! -d "bin" ]; then mkdir -p bin; fi
+	wget -q -O - $XDATA_URL | (xzcat -) | (tar -xvf - $XDATA_DIR)
+	mv $XDATA_DIR bin
+	rmdir -p $XDATA_BIN
+fi
