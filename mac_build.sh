0a28b39fa53ddefca4e4a8b8d9c17af0e29fcc11
Sergey Chaban <sergey.chaban@gmail.com>
2022-01-25 14:24:43 +0200

    small changes in macOS build script;

--- mac_build.sh
+++ mac_build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 if [ ! -d "tmp/obj" ]; then mkdir -p tmp/obj; fi
 if [ ! -d "bin/mac" ]; then mkdir -p bin/mac; fi
-MAC_FWS="-framework OpenGL -framework Foundation -framework Cocoa"
+MAC_EXE=bin/mac/x.out
+MAC_LIBS="-framework OpenGL -framework Foundation -framework Cocoa"
+rm -f $MAC_EXE
 clang -I src/mac src/mac/mac_main.m -c -o tmp/obj/mac_main.o
-clang++ -std=c++11 -I src/mac -DDEF_DEMO="\"roof\"" `ls src/*.cpp` tmp/obj/mac_main.o -o bin/mac/x.out $MAC_FWS $*
+clang++ -std=c++11 -I src/mac -DDEF_DEMO="\"roof\"" `ls src/*.cpp` tmp/obj/mac_main.o -o $MAC_EXE $MAC_LIBS $*
+echo "-> exe: " `ls -o $MAC_EXE`
