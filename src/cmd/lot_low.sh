df22cd23a978b6508301340d9348c9bcdb309dba
Sergey Chaban <sergey.chaban@gmail.com>
2022-01-28 11:11:27 +0200

    set number of worker threads to active CPU count; run in 40-chara mode by default;

--- src/cmd/lot_low.sh
+++ src/cmd/lot_low.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-./crosscore_demo -demo:lot -draw:ogl -bump:0 -spec:0 -w:720 -h:480 -smap:256 -tlod_bias:0 -vl:1 -lowq:1 -adapt:1 $*
+NWRK=4
+SYSNAME=`uname -s`
+case $SYSNAME in
+	Linux)
+		NWRK=$(nproc)
+	;;
+	OpenBSD)
+		NWRK=`sysctl -n hw.ncpuonline`
+	;;
+esac
+./crosscore_demo -nwrk:$NWRK -demo:lot -mode:1 -draw:ogl -bump:0 -spec:0 -w:720 -h:480 -smap:256 -tlod_bias:0 -vl:1 -lowq:1 -adapt:1 $*
