08dea8bdd9bb741577946dc1b20170bd783ae905
Sergey Chaban <sergey.chaban@gmail.com>
2022-02-02 14:49:22 +0200

    + vema build script;

---
+++ xvema.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+VEMA_DIR=etc/vema
+if [ ! -d "$VEMA_DIR" ]; then
+	mkdir -p $VEMA_DIR
+fi
+
+VEMA_URL=https://raw.githubusercontent.com/schaban/schaban.github.io/main/vema
+for srcfile in vema.c vema.h
+do
+	if [ ! -f "$VEMA_DIR/$srcfile" ]; then
+		echo "Downloading $srcfile"
+		curl $VEMA_URL/$srcfile -o $VEMA_DIR/$srcfile
+	fi
+done
+
+OUT_DIR=bin/xvema
+if [ ! -d "$OUT_DIR" ]; then
+	mkdir -p $OUT_DIR
+fi
+X_EXE=$OUT_DIR/crosscore_demo
+rm -f $X_EXE
+# gcc|clang
+case 0 in
+	0)
+		_CC_=gcc
+		_CXX_=g++
+	;;
+	1)
+		_CC_=clang
+		_CXX_=clang++
+	;;
+esac
+echo "Compiling with "$_CC_"/"$_CXX_
+
+VEMA_ARCH=""
+case `uname -m` in
+	x86_64)
+		VEMA_ARCH="-DVEMA_GCC_SSE"
+	;;
+	aarch64)
+		VEMA_ARCH="-DVEMA_GCC_AARCH64_ASM -DVEMA_ARM_VSQRT"
+	;;
+esac
+
+OPTI_FLGS="-march=native -O3 -ffast-math -ftree-vectorize -flto"
+VEMA_FLGS="-DXD_USE_VEMA -DVEMA_NO_CLIB $VEMA_ARCH"
+$_CC_ -std=c99 $OPTI_FLGS $VEMA_FLGS $VEMA_DIR/vema.c -g -c -o $VEMA_DIR/vema.o
+$_CXX_ -std=c++11 $OPTI_FLGS $VEMA_FLGS -DOGLSYS_ES=0 -DOGLSYS_CL=0 -DDRW_NO_VULKAN=1 -DXD_TSK_NATIVE=1 -g -I src -I inc -I $VEMA_DIR -DX11 `ls src/*.cpp` $VEMA_DIR/vema.o -o $X_EXE -ldl -lX11 -lpthread
+objdump -d $X_EXE > $X_EXE.txt
+ls -ho --color=always $X_EXE
