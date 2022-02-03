d0184e4bb043b76203d4ca36b026771192989c05
Sergey Chaban <sergey.chaban@gmail.com>
2022-02-03 16:24:39 +0200

    handle 32-bit arm builds;

--- mk_lib_links.sh
+++ mk_lib_links.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 if [ ! -d "lib" ]; then mkdir -p lib; fi
+
+case `uname -m` in
+	armv7l)
+		libDir=/lib/arm-linux-gnueabihf
+	;;
+	*)
+		libDir=/lib/`uname -m`-linux-gnu
+	;;
+esac
+
 for lib in X11 EGL GLESv2
 do
-	libBase=/lib/`uname -m`-linux-gnu/lib$lib.so
+	libBase=$libDir/lib$lib.so
 	libPath=`ls -1 $libBase.[0-9]*.* | head -1`
 	echo $libPath
 	ln -s $libPath ./lib/lib$lib.so
