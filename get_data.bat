a3c5c22a06dce09f5effa6dcb813094279c2a732
schaban <sergey.chaban@gmail.com>
2022-03-27 13:37:38 +0300

    + native data downloader for Windows;

---
+++ get_data.bat
@@ -0,0 +1,13 @@
+@echo off
+set XDATA_ARC=xcore_web.tar.xz
+set XDATA_URL=https://schaban.github.io/crosscore_web_demo/%XDATA_ARC%
+set XDATA_TMP=xcore_web
+
+if not exist bin mkdir bin
+if not exist %XDATA_TMP% mkdir %XDATA_TMP%
+
+curl %XDATA_URL% -o %XDATA_TMP%/%XDATA_ARC%
+tar -xvJf %XDATA_TMP%/%XDATA_ARC% %XDATA_TMP%/bin/data
+move /Y %XDATA_TMP%/bin/data bin
+rmdir /Q /S %XDATA_TMP%
+
