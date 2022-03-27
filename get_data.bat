eab0d59a02d0be7e40ac9cfd5f41c63d3e684fee
schaban <sergey.chaban@gmail.com>
2022-03-27 23:48:42 +0300

    Windows data dl: use full paths for curl/tar;

--- get_data.bat
+++ get_data.bat
@@ -1,6 +1,12 @@
 @echo off
 
-if not x%1==x set PATH=%1
+set _CURL_=curl
+set _TAR_=tar
+if not x%1==x (
+	set PATH=%1
+	set _CURL_=%1\curl.exe
+	set _TAR_=%1\tar.exe
+)
 
 set XDATA_ARC=xcore_web.tar.xz
 set XDATA_URL=https://schaban.github.io/crosscore_web_demo/%XDATA_ARC%
@@ -9,8 +15,8 @@ set XDATA_TMP=xcore_web
 if not exist bin mkdir bin
 if not exist %XDATA_TMP% mkdir %XDATA_TMP%
 
-curl %XDATA_URL% -o %XDATA_TMP%/%XDATA_ARC%
-tar -xvJf %XDATA_TMP%/%XDATA_ARC% %XDATA_TMP%/bin/data
+%_CURL_% %XDATA_URL% -o %XDATA_TMP%/%XDATA_ARC%
+%_TAR_% -xvJf %XDATA_TMP%/%XDATA_ARC% %XDATA_TMP%/bin/data
 move /Y %XDATA_TMP%\bin\data bin
 rmdir /Q /S %XDATA_TMP%
 
