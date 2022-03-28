cb28d978dd799ea54415722004f7224471daa82d
Sergey Chaban <sergey.chaban@gmail.com>
2022-03-28 13:57:09 +0300

    data dl: handle MINGW and CYGWIN platforms;

--- get_data.sh
+++ get_data.sh
@@ -5,19 +5,23 @@ XDATA_TMP="xcore_web"
 XDATA_BIN="$XDATA_TMP/bin"
 XDATA_DIR="$XDATA_BIN/data"
 
-if [ "$(uname -s)" = "Linux" ]; then
-	wget -q -O - $XDATA_URL | (tar --strip-components=1 -xvJf - $XDATA_DIR)
-elif [ "$(uname -s)" = "Darwin" ]; then
-	if [ ! -d "bin" ]; then mkdir -p bin; fi
-	if [ ! -d "$XDATA_TMP" ]; then mkdir -p $XDATA_TMP; fi
-	curl $XDATA_URL -o $XDATA_TMP/$XDATA_ARC
-	tar -xvJf $XDATA_TMP/$XDATA_ARC $XDATA_DIR
-	mv $XDATA_DIR bin
-	rm -f $XDATA_TMP/$XDATA_ARC
-	rmdir -p $XDATA_BIN
-else
-	if [ ! -d "bin" ]; then mkdir -p bin; fi
-	wget -q -O - $XDATA_URL | (xzcat -) | (tar -xvf - $XDATA_DIR)
-	mv $XDATA_DIR bin
-	rmdir -p $XDATA_BIN
-fi
+case `uname -s` in
+	Linux)
+		wget -q -O - $XDATA_URL | (tar --strip-components=1 -xvJf - $XDATA_DIR)
+	;;
+	Darwin | MINGW* | CYGWIN*)
+		if [ ! -d "bin" ]; then mkdir -p bin; fi
+		if [ ! -d "$XDATA_TMP" ]; then mkdir -p $XDATA_TMP; fi
+		curl $XDATA_URL -o $XDATA_TMP/$XDATA_ARC
+		tar -xvJf $XDATA_TMP/$XDATA_ARC $XDATA_DIR
+		mv $XDATA_DIR bin
+		rm -f $XDATA_TMP/$XDATA_ARC
+		rmdir -p $XDATA_BIN
+	;;
+	*)
+		if [ ! -d "bin" ]; then mkdir -p bin; fi
+		wget -q -O - $XDATA_URL | (xzcat -) | (tar -xvf - $XDATA_DIR)
+		mv $XDATA_DIR bin
+		rmdir -p $XDATA_BIN
+	;;
+esac
