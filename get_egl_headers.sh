52e1472dd1cbb0b15b6b27c6666b05f1ae06ed14
Sergey Chaban <sergey.chaban@gmail.com>
2022-07-16 12:42:19 +0300

    Unix build: updated khronos registry url;

--- get_egl_headers.sh
+++ get_egl_headers.sh
@@ -2,6 +2,6 @@
 if [ ! -d "inc/EGL" ]; then mkdir -p inc/GL; fi
 for kh in egl.h eglext.h eglplatform.h
 do
-	wget -P inc/EGL https://www.khronos.org/registry/EGL/api/EGL/$kh
+	wget -P inc/EGL https://registry.khronos.org/EGL/api/EGL/$kh
 done
 
--- get_ogl_headers.sh
+++ get_ogl_headers.sh
@@ -2,7 +2,7 @@
 if [ ! -d "inc/GL" ]; then mkdir -p inc/GL; fi
 for kh in glext.h glcorearb.h wglext.h
 do
-	wget -P inc/GL https://www.khronos.org/registry/OpenGL/api/GL/$kh
+	wget -P inc/GL https://registry.khronos.org/OpenGL/api/GL/$kh
 done
 if [ ! -d "inc/KHR" ]; then mkdir -p inc/KHR; fi
-wget -P inc/KHR https://www.khronos.org/registry/EGL/api/KHR/khrplatform.h
+wget -P inc/KHR https://registry.khronos.org/EGL/api/KHR/khrplatform.h
