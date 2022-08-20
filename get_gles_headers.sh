7067144232b690e5bcf6c26026389bf3f2c68833
Sergey Chaban <sergey.chaban@gmail.com>
2022-08-20 11:17:21 +0300

    headers d/l: replace existing files;

--- get_gles_headers.sh
+++ get_gles_headers.sh
@@ -2,11 +2,11 @@
 if [ ! -d "inc/GLES2" ]; then mkdir -p inc/GLES2; fi
 for kh in gl2.h gl2ext.h gl2platform.h
 do
-	wget -P inc/GLES2 https://registry.khronos.org/OpenGL/api/GLES2/$kh
+	wget -O inc/GLES2/$kh https://registry.khronos.org/OpenGL/api/GLES2/$kh
 done
 
 if [ ! -d "inc/GLES3" ]; then mkdir -p inc/GLES3; fi
 for kh in gl3.h gl31.h gl32.h gl3platform.h
 do
-	wget -P inc/GLES3 https://registry.khronos.org/OpenGL/api/GLES3/$kh
+	wget -O inc/GLES3/$kh https://registry.khronos.org/OpenGL/api/GLES3/$kh
 done
--- get_ogl_headers.sh
+++ get_ogl_headers.sh
@@ -2,7 +2,7 @@
 if [ ! -d "inc/GL" ]; then mkdir -p inc/GL; fi
 for kh in glext.h glcorearb.h wglext.h
 do
-	wget -P inc/GL https://registry.khronos.org/OpenGL/api/GL/$kh
+	wget -O inc/GL/$kh https://registry.khronos.org/OpenGL/api/GL/$kh
 done
 if [ ! -d "inc/KHR" ]; then mkdir -p inc/KHR; fi
-wget -P inc/KHR https://registry.khronos.org/EGL/api/KHR/khrplatform.h
+wget -O inc/KHR/khrplatform.h https://registry.khronos.org/EGL/api/KHR/khrplatform.h
