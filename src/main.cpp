b19cc4f5566a660b4debb2cd04e0d2c769e203dd
Sergey Chaban <sergey.chaban@gmail.com>
2022-09-17 18:08:37 +0300

    + ROM-archive support;

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,122 @@ void wi_set_key_state(const char* pName, const int state) {
 
 } // wi
 
+#ifdef USE_XROM_ARCHIVE
+
+#define XROM_SIG XD_FOURCC('X', 'R', 'O', 'M')
+#define XROM_PATH_PREFIX "rom:"
+
+struct ROMHead {
+	uint32_t sig;
+	uint32_t size;
+	uint32_t hsize;
+	uint32_t nfiles;
+};
+
+struct ROMFileInfo {
+	struct Path {
+		uint32_t tag; /* len, hash */
+		char str[60];
+
+		void reset() {
+			tag = 0;
+			for (size_t i = 0; i < sizeof(str); ++i) {
+				str[i] = 0;
+			}
+		}
+
+		void set(const char* pStr) {
+			reset();
+			if (pStr) {
+				size_t len = nxCore::str_len(pStr);
+				if (len <= sizeof(str)) {
+					tag = uint32_t((uint16_t)len) | (uint32_t(nxCore::str_hash16(pStr)) << 16);
+					nxCore::mem_copy(str, pStr, len);
+				}
+			}
+		}
+		size_t len() const {
+			return nxCalc::min(int(tag & 0xFFFF), int(sizeof(str)));
+		}
+	};
+
+	Path path;
+	uint32_t offs;
+	uint32_t size;
+	uint32_t reserved;
+	uint32_t reserved2;
+};
+
+extern "C" uint8_t _binary_xrom_start;
+static ROMHead* s_pROM = (ROMHead*)&_binary_xrom_start;
+
+XD_NOINLINE static bool rom_valid() {
+	return s_pROM && (s_pROM->sig == XROM_SIG);
+}
+
+static xt_fhandle rom_fopen(const char* pPath) {
+	xt_fhandle fh = nullptr;
+	if (pPath && rom_valid()) {
+		ROMHead* pROM = s_pROM;
+		const char* pPrefix = "./" XROM_PATH_PREFIX "/";
+		if (nxCore::str_starts_with(pPath, pPrefix)) {
+			const char* pSearchPath = pPath + nxCore::str_len(pPrefix);
+			ROMFileInfo::Path search;
+			search.set(pSearchPath);
+			ROMFileInfo* pInfo = nullptr;
+			ROMFileInfo* pInfoROM = (ROMFileInfo*)(pROM + 1);
+			for (uint32_t i = 0; i < pROM->nfiles; ++i) {
+				if (pInfoROM[i].path.tag == search.tag) {
+					if (nxCore::mem_eq(pInfoROM[i].path.str, search.str, search.len())) {
+						fh = (xt_fhandle)&pInfoROM[i];
+						break;
+					}
+				}
+			}
+		}
+	}
+	return fh;
+}
+
+static void rom_fclose(xt_fhandle fh) {
+}
+
+static size_t rom_fsize(xt_fhandle fh) {
+	size_t size = 0;
+	if (fh && rom_valid()) {
+		ROMHead* pROM = s_pROM;
+		ROMFileInfo* pInfoTop = (ROMFileInfo*)(pROM + 1);
+		ROMFileInfo* pInfoEnd = pInfoTop + pROM->nfiles;
+		ROMFileInfo* pInfo = (ROMFileInfo*)fh;
+		if (pInfo >= pInfoTop && pInfo < pInfoEnd) {
+			size = pInfo->size;
+		} else {
+			nxCore::dbg_msg("rom_fsize: invalid handle\n");
+		}
+	}
+	return size;
+}
+
+static size_t rom_fread(xt_fhandle fh, void* pDst, size_t nbytes) {
+	size_t nread = 0;
+	if (rom_valid() && fh && pDst && nbytes > 0) {
+		ROMHead* pROM = s_pROM;
+		ROMFileInfo* pInfoTop = (ROMFileInfo*)(pROM + 1);
+		ROMFileInfo* pInfoEnd = pInfoTop + pROM->nfiles;
+		ROMFileInfo* pInfo = (ROMFileInfo*)fh;
+		if (pInfo >= pInfoTop && pInfo < pInfoEnd) {
+			void* pSrc = XD_INCR_PTR(pROM, pInfo->offs);
+			nread = pInfo->size;
+			nxCore::mem_copy(pDst, pSrc, nread);
+		} else {
+			nxCore::dbg_msg("rom_fread: invalid handle\n");
+		}
+	}
+	return nread;
+}
+#endif // USE_XROM_ARCHIVE
+
+
 static void dbgmsg_impl(const char* pMsg) {
 	::printf("%s", pMsg);
 	::fflush(stdout);
@@ -48,7 +164,16 @@ static void init_sys() {
 	sxSysIfc sysIfc;
 	nxCore::mem_zero(&sysIfc, sizeof(sysIfc));
 	sysIfc.fn_dbgmsg = dbgmsg_impl;
+#ifdef USE_XROM_ARCHIVE
+	sysIfc.fn_fopen = rom_fopen;
+	sysIfc.fn_fclose = rom_fclose;
+	sysIfc.fn_fread = rom_fread;
+	sysIfc.fn_fsize = rom_fsize;
+#endif
 	nxSys::init(&sysIfc);
+}
+
+static void reset_sys() {
 }
 
 static void* oglsys_mem_alloc(size_t size, const char* pTag) {
@@ -101,7 +226,13 @@ static void init_scn(const char* pAppPath) {
 #endif
 	;
 
-	const char* pAltDataDir = nxApp::get_opt("data");
+	const char* pAltDataDir =
+#ifdef USE_XROM_ARCHIVE
+	XROM_PATH_PREFIX
+#else
+	nxApp::get_opt("data")
+#endif
+	;
 	ScnCfg scnCfg;
 	scnCfg.set_defaults();
 	if (pAltDataDir) {
