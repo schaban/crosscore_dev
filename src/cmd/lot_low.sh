adc885f80fa50ffeee272675f9a7031234985278
Sergey Chaban <sergey.chaban@gmail.com>
2022-10-06 12:27:05 +0300

    lot_low: report kernel scheduler name on FreeBSD;

--- src/cmd/lot_low.sh
+++ src/cmd/lot_low.sh
@@ -10,6 +10,7 @@ case $SYSNAME in
 	;;
 	FreeBSD)
 		NWRK=`sysctl -n kern.smp.cpus`
+		echo "FreeBSD scheduler: `sysctl -n kern.sched.name`"
 	;;
 esac
 ./crosscore_demo -nwrk:$NWRK -demo:lot -mode:1 -draw:ogl -bump:0 -spec:0 -w:720 -h:480 -smap:256 -tlod_bias:0 -vl:1 -lowq:1 -adapt:1 $*
