de72cf3571ac4d8e6b1422d67f7152f3f1403121
Sergey Chaban <sergey.chaban@gmail.com>
2023-03-12 12:20:34 +0200

    lot benchmark launcher: set number of workers to number of CPUs on illumos;

--- src/cmd/lot_low.sh
+++ src/cmd/lot_low.sh
@@ -12,6 +12,12 @@ case $SYSNAME in
 		NWRK=`sysctl -n kern.smp.cpus`
 		echo "FreeBSD scheduler: `sysctl -n kern.sched.name`"
 	;;
+	SunOS)
+		NCPUS=`psrinfo -t`
+		NCORES=`psrinfo -tc`
+		echo "$NCPUS CPUs on $NCORES cores"
+		NWRK=$NCPUS
+	;;
 esac
 PROGDIR=`dirname $0`
 $PROGDIR/crosscore_demo -nwrk:$NWRK -demo:lot -mode:1 -draw:ogl -bump:0 -spec:0 -w:720 -h:480 -smap:256 -tlod_bias:0 -vl:1 -lowq:1 -adapt:1 $*
