da41df35e792328712b159ea1fcc1f7877bea924
Sergey Chaban <sergey.chaban@gmail.com>
2023-03-13 09:33:53 +0200

    xbuild: + vema mode;

--- xbuild.sh
+++ xbuild.sh
@@ -47,6 +47,41 @@ case $SYS_NAME in
 	;;
 esac
 
+DL_CMD=""
+if [ -x "`command -v wget`" ]; then
+	DL_CMD="wget -O"
+elif [ -x "`command -v curl`" ]; then
+	DL_CMD="curl -o"
+fi
+
+VEMA_DIR=etc/vema
+VEMA_SRCS=""
+VEMA_OPTS=""
+USE_VEMA=${XBUILD_VEMA:-0}
+if [ $USE_VEMA -ne 0 ]; then
+	if [ -n "$DL_CMD" ]; then
+		mkdir -p $VEMA_DIR
+		VEMA_URL=https://schaban.github.io/vema
+		if [ ! -f "$VEMA_DIR/vema.h" ]; then
+			echo "Downloading vema header..."
+			$DL_CMD $VEMA_DIR/vema.h $VEMA_URL/vema.h
+		fi
+		if [ ! -f "$VEMA_DIR/vema.cpp" ]; then
+			echo "Downloading vema source..."
+			$DL_CMD $VEMA_DIR/vema.cpp $VEMA_URL/vema.c
+		fi
+		if [ -f "$VEMA_DIR/vema.h" ] && [ -f "$VEMA_DIR/vema.cpp" ]; then
+			echo "Using vema."
+			VEMA_SRCS="$VEMA_DIR/vema.cpp"
+			VEMA_OPTS="-I $VEMA_DIR -DXD_USE_VEMA -DVEMA_NO_CLIB"
+		else
+			USE_VEMA=0
+		fi
+	else
+		USE_VEMA=0
+	fi
+fi
+
 CXX=${CXX:-$DEF_CXX}
 
 DISASM_OPTS=""
@@ -57,7 +92,7 @@ case $SYS_ARCH in
 esac
 
 echo "Compiling with $CXX..."
-$CXX $CXX_OPTS $PROG_OPTS $PROG_INCS $PROG_SRCS $PROG_LIBS -o $PROG_PATH $*
+$CXX $CXX_OPTS $VEMA_OPTS $PROG_OPTS $PROG_INCS $VEMA_SRCS $PROG_SRCS $PROG_LIBS -o $PROG_PATH $*
 MSG="Failed."
 if [ -f $PROG_PATH ]; then
 	objdump $DISASM_OPTS -dC $PROG_PATH > $PROG_PATH.txt
