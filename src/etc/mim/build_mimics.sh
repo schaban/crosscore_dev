a99cfac685635d8c05e7f60e2c5d33ec5e06f173
Sergey Chaban <sergey.chaban@gmail.com>
2023-10-09 19:48:02 +0300

    mim: place compiled mimic libs into separate directory;

--- src/etc/mim/build_mimics.sh
+++ src/etc/mim/build_mimics.sh
@@ -2,11 +2,17 @@
 DEF_CC=~/Tools/crosscompile/gcc-aarch64/bin/aarch64-none-linux-gnu-gcc
 CC=${CC:-$DEF_CC}
 
+LIB_DIR=lib
+
+if [ ! -d $LIB_DIR ]; then
+	mkdir -p $LIB_DIR
+fi
+
 echo "Compiling with $CC..."
 
 for lib in drm gbm EGL GLESv2; do
 	SRC=mimic_$lib.c
-	DST=lib$lib.so
+	DST=$LIB_DIR/lib$lib.so
 	echo $SRC "->" $DST
 	$CC -shared $SRC -o $DST
 done
