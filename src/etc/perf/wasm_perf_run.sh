1724652e49159a2e9b1267784441281619e728b2
Sergey Chaban <sergey.chaban@gmail.com>
2023-10-29 13:13:29 +0200

    perf wasm runner: + var to override the default nodejs path;

--- src/etc/perf/wasm_perf_run.sh
+++ src/etc/perf/wasm_perf_run.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+XNODE=${XNODE:-node}
+
 clear
 
 echo
 echo -------- wasm NxN mul
-node $* perf_nnmul.js -nmuls:100
+$XNODE $* perf_nnmul.js -nmuls:100
 
 echo
 echo -------- wasm seg intersect
-node $* perf_isect.js
+$XNODE $* perf_isect.js
 
 echo
 echo -------- wasm make AABB-tree
-node $* perf_mkbvh.js -n:100000
+$XNODE $* perf_mkbvh.js -n:100000
 
 echo
 echo -------- SH pano
-node $* perf_shpano.js -w:1024 -h:512
+$XNODE $* perf_shpano.js -w:1024 -h:512
