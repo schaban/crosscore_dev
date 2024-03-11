63178fb9d93a81a19c54efbc3c6ad4f60f4fa8fa
Sergey Chaban <sergey.chaban@gmail.com>
2024-03-11 11:15:16 +0200

    vk_headers: use git;

--- get_vk_headers.sh
+++ get_vk_headers.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-VK_H_REPO="https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vulkan"
-VK_INC="inc/vulkan"
-if [ ! -d "$VK_INC" ]; then mkdir -p $VK_INC; fi
-for kh in vulkan.h vk_platform.h vulkan_core.h vulkan_win32.h vulkan_xlib.h
-do
-	wget -O $VK_INC/$kh $VK_H_REPO/$kh
-done
 
+TMP_DIR=tmp
+INC_DIR=inc
+
+if [ ! -d "$TMP_DIR" ]; then mkdir -p $TMP_DIR; fi
+if [ ! -d "$INC_DIR" ]; then mkdir -p $INC_DIR; fi
+
+BUILD_DIR=$PWD
+
+cd $TMP_DIR
+git clone --depth 1 https://github.com/KhronosGroup/Vulkan-Headers.git
+cd $BUILD_DIR
+cp -r $TMP_DIR/Vulkan-Headers/include/vk_video $INC_DIR
+cp -r $TMP_DIR/Vulkan-Headers/include/vulkan $INC_DIR
