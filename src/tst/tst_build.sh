2d5e0f493b3e5c8bafe88804c21fd5c7add665b1
schaban <sergey.chaban@gmail.com>
2025-07-01 09:34:06 +0300

    tst_build: + comment with suggested options for x86 and aarch;

--- src/tst/tst_build.sh
+++ src/tst/tst_build.sh
@@ -3,6 +3,10 @@
 DEF_CXX="g++"
 CXX=${CXX:-$DEF_CXX}
 
+# -DXD_HAS_F16=1
+# x86_64: -mf16c -mfma
+# aarch64: -march=armv8.2-a+fp16
+
 OPTI_OPTS="-O3 -ffast-math -flto"
 
 CXX_CMD="$CXX -pthread -I .. ../crosscore.cpp $OPTI_OPTS"
