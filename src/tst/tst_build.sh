9c7dcc77cb150fe94343eeaa68c79e2f2a18de0e
schaban <sergey.chaban@gmail.com>
2025-07-02 15:47:56 +0300

    tst_build: set standard to c++11;

--- src/tst/tst_build.sh
+++ src/tst/tst_build.sh
@@ -9,6 +9,6 @@ CXX=${CXX:-$DEF_CXX}
 
 OPTI_OPTS="-O3 -ffast-math -flto"
 
-CXX_CMD="$CXX -pthread -I .. ../crosscore.cpp $OPTI_OPTS"
+CXX_CMD="$CXX -pthread -std=c++11 -I .. ../crosscore.cpp $OPTI_OPTS"
 
 $CXX_CMD tst_nnmul_h.cpp -o tst_nnmul_h $*
